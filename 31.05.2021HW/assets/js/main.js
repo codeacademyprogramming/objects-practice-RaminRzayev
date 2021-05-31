cf8c706429dbbc372856d439c095c6209dd170ac
RaminRzayev <raminag@code.edu.az>
2021-05-31 13:54:51 +0400

    HW

---
+++ 31.05.2021HW/assets/js/main.js
@@ -0,0 +1,37 @@
+// // HELLO OBJECT
+// let user = new Object();
+// user.name= "Dadas";
+// user.surname="Dadaszade";
+// user.name= "Ali";
+// delete user.surname;
+
+// console.log(user)
+
+// SUM OBJECT
+// let developers = new Object();
+// developers.Qabil=4000;
+// developers.Nergiz=3500;
+// developers.Ramin=700;
+// let cemMaas=0;
+
+// for(let sira in developers){
+//     cemMaas+=developers[sira];
+// }
+// console.log(cemMaas)
+
+// MULTIPLY NUMERIC
+
+// let menu = new Object();
+// menu.width = 200;
+// menu.height = 300;
+// menu.title = "My menu";
+
+// function multiplyNumeric(menu) {
+//     for (let key in menu) {
+//         if (typeof menu[key] == 'number') {
+//             menu[key] *= 2;
+//         }
+//     }
+// }
+// multiplyNumeric(menu);
+// console.log(menu);
